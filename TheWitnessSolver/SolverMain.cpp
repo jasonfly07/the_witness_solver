5416051e3feb22d57d68753e93b3021d9ee30846
jasonfly07 <shengchieh1989@gmail.com>
2016-02-07 23:05:08 -0800

    set up 3 tests for black/white separation; last one is too slow

--- TheWitnessSolver/SolverMain.cpp
+++ TheWitnessSolver/SolverMain.cpp
@@ -18,6 +18,7 @@ int main() {
   p1.AddNodeObstacle(Vector2(0, 0), Vector2(0, 1));
   p1.Solve();
   assert(p1.GetPaths().size() == 14);
+  std::cout << "puzzle 1 solved" << std::endl;
   // TODO: check if there're duplicated paths
 
   // Maze with essential nodes
@@ -37,29 +38,59 @@ int main() {
   p2.AddEssentialNode(Vector2(3, 3));
   p2.Solve();
   assert(p2.GetPaths().size() == 1);
+  std::cout << "puzzle 2 solved" << std::endl;
 
   // Black & white separation
-  Puzzle p3(5, 5);
+  Puzzle p3(3, 3);
+  p3.AddHead(Vector2(2, 0));
+  p3.AddTail(Vector2(0, 2));
   p3.SetBlockType(Vector2(0, 0), Black);
-  p3.SetBlockType(Vector2(0, 3), Black);
+  p3.SetBlockType(Vector2(0, 1), Black);
   p3.SetBlockType(Vector2(1, 0), Black);
-  p3.SetBlockType(Vector2(1, 1), Black);
-  p3.SetBlockType(Vector2(1, 2), Black);
-  p3.SetBlockType(Vector2(1, 3), Black);
-  p3.SetBlockType(Vector2(2, 0), Black);
-  p3.SetBlockType(Vector2(2, 2), Black);
-  p3.SetBlockType(Vector2(2, 3), Black);
-  p3.SetBlockType(Vector2(3, 3), Black);
-  p3.SetBlockType(Vector2(0, 2), White);
-  p3.SetBlockType(Vector2(2, 1), White);
-  p3.SetBlockType(Vector2(3, 0), White);
-  p3.SetBlockType(Vector2(3, 1), White);
-  p3.SetBlockType(Vector2(3, 2), White);
+  p3.SetBlockType(Vector2(1, 1), White);
+  p3.Solve();
+  assert(p3.GetPaths().size() == 1);
+  std::cout << "puzzle 3 solved" << std::endl;
 
-  p3.AddBlockObstacle(Vector2(2, 1), Vector2(2, 2));
-  for (auto& block : p3.GetBlock(2, 1).neighborSet) {
-    std::cout << block->coord << std::endl;
-  }
+  // Black & white separation
+  Puzzle p4(4, 4);
+  p4.AddHead(Vector2(3, 0));
+  p4.AddTail(Vector2(2, 0));
+  p4.SetBlockType(Vector2(0, 0), Black);
+  p4.SetBlockType(Vector2(0, 1), Black);
+  p4.SetBlockType(Vector2(0, 2), Black);
+  p4.SetBlockType(Vector2(1, 0), Black);
+  p4.SetBlockType(Vector2(1, 2), Black);
+  p4.SetBlockType(Vector2(1, 1), White);
+  p4.SetBlockType(Vector2(2, 0), White);
+  p4.SetBlockType(Vector2(2, 1), White);
+  p4.SetBlockType(Vector2(2, 2), White);
+  p4.Solve();
+  assert(p4.GetPaths().size() == 1);
+  std::cout << "puzzle 4 solved" << std::endl;
+
+  // Black & white separation
+  Puzzle p5(5, 5);
+  p5.AddHead(Vector2(4, 0));
+  p5.AddTail(Vector2(0, 1));
+  p5.SetBlockType(Vector2(0, 0), Black);
+  p5.SetBlockType(Vector2(0, 3), Black);
+  p5.SetBlockType(Vector2(1, 0), Black);
+  p5.SetBlockType(Vector2(1, 1), Black);
+  p5.SetBlockType(Vector2(1, 2), Black);
+  p5.SetBlockType(Vector2(1, 3), Black);
+  p5.SetBlockType(Vector2(2, 0), Black);
+  p5.SetBlockType(Vector2(2, 2), Black);
+  p5.SetBlockType(Vector2(2, 3), Black);
+  p5.SetBlockType(Vector2(3, 3), Black);
+  p5.SetBlockType(Vector2(0, 2), White);
+  p5.SetBlockType(Vector2(2, 1), White);
+  p5.SetBlockType(Vector2(3, 0), White);
+  p5.SetBlockType(Vector2(3, 1), White);
+  p5.SetBlockType(Vector2(3, 2), White);
+  p5.Solve();
+  std::cout << p5.GetPaths().size() << std::endl;
+  std::cout << "puzzle 5 solved" << std::endl;
 
   return 0;
 }
