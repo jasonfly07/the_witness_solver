5aa1eae4ec2155565d8862f1716f91a359354b12
jasonfly07 <shengchieh1989@gmail.com>
2016-02-14 23:00:51 -0800

    restore some old codes in Puzzle

--- TheWitnessSolver/Puzzle.cpp
+++ TheWitnessSolver/Puzzle.cpp
@@ -52,3 +52,44 @@ void Puzzle::AddEssentialSide(const Vector2& vec1, const Vector2& vec2) {
 void Puzzle::SetBlockType(const Vector2& vec, BlockType type) {
   m_BlockMap.SetType(vec, type);
 }
+
+bool Puzzle::CheckBlackWhiteBlocks() {
+  for (int r = 0; r < BlockRows(); r++) {
+    for (int c = 0; c < BlockCols(); c++) {
+      const Block& currBlock = GetBlock(r, c);
+      if ((currBlock.type == Black) || (currBlock.type == White)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void Puzzle::PreprocessBlackWhiteBlocks() {
+  // For every block [r, c], process the sides (if valid):
+  // 1. between [r, c] and [r, c + 1]
+  // 2. between [r, c] and [r + 1, c]
+  for (int r = 0; r < BlockRows(); r++) {
+    for (int c = 0; c < BlockCols(); c++) {
+      const Block& currBlock = GetBlock(r, c);
+
+      // 1. [r, c + 1]
+      if (ValidBlockCoord(r, c + 1)) {
+        const Block& rightBlock = GetBlock(r, c + 1);
+        if ((currBlock.type == White && rightBlock.type == Black) ||
+            (currBlock.type == Black && rightBlock.type == White)) {
+          m_SideEssentials.insert(Side(&GetNode(r, c + 1), &GetNode(r + 1, c + 1)));
+        }
+      }
+
+      // 2. [r + 1, c]
+      if (ValidBlockCoord(r + 1, c)) {
+        const Block& bottomBlock = GetBlock(r + 1, c);
+        if ((currBlock.type == White && bottomBlock.type == Black) ||
+            (currBlock.type == Black && bottomBlock.type == White)) {
+          m_SideEssentials.insert(Side(&GetNode(r + 1, c), &GetNode(r + 1, c + 1)));
+        }
+      }
+    }
+  }
+}
--- TheWitnessSolver/Puzzle.h
+++ TheWitnessSolver/Puzzle.h
@@ -27,11 +27,15 @@ public:
   inline size_t NodeCols() const { return m_NodeMap.Cols(); }
   inline NodePtrSet& GetHeads() { return m_NodeHeads; }
   inline NodePtrSet& GetTails() { return m_NodeTails; }
+  inline NodePtrSet& GetEssentialNodes() { return m_NodeEssentials; }
+  inline SideSet& GetEssentialSides() { return m_SideEssentials; }
 
   inline Block& GetBlock(int r, int c)       { return m_BlockMap.GetBlock(r, c); }
   inline Block& GetBlock(const Vector2& vec) { return m_BlockMap.GetBlock(vec); }
   inline size_t BlockRows() const { return m_BlockMap.Rows(); }
   inline size_t BlockCols() const { return m_BlockMap.Cols(); }
+
+  inline BlockMap& GetBlockMap() { return m_BlockMap; }
 
   // Check the validity of coordinates
   inline bool ValidNodeCoord(const Vector2& v) const  { return m_NodeMap.ValidCoord(v);     }
@@ -55,6 +59,15 @@ public:
 
   // Set the type of a block
   void SetBlockType(const Vector2& vec, BlockType type);
+
+  // Check if the puzzle has black & white puzzles
+  // Expensive, so this should only be called once before solving
+  bool CheckBlackWhiteBlocks();
+  void PreprocessBlackWhiteBlocks();
+
+  bool HasEssentialNode() {
+    return m_NodeEssentials.size() > 0;
+  }
 
 private:
 
