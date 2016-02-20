b5707e034dc259a1473128a8df81e1303828dce7
jasonfly07 <shengchieh1989@gmail.com>
2016-02-19 22:13:04 -0800

    comment out tails check temporarily

--- TheWitnessSolver/Path.cpp
+++ TheWitnessSolver/Path.cpp
@@ -165,16 +165,16 @@ bool Path::EvaluateSegment(const BlockPtrSet& segment) {
   // Are there unvisited tails & no tail up ahead?
   // If both true, return false immediately
   // TODO: verify that this check works
-  if (m_PuzzlePtr->HasEssentialNode()) {
-    for (const auto& nodePtr : unvisitedNodes) {
-      if (nodePtr->isTail) {
-        m_MissedTailCount++;
-      }
-      if ((m_MissedTailCount + m_VisitedTails.size()) >= m_PuzzlePtr->GetTails().size()) {
-        return false;
-      }
-    }
-  }
+  //if (m_PuzzlePtr->HasEssentialNode()) {
+  //  for (const auto& nodePtr : unvisitedNodes) {
+  //    if (nodePtr->isTail) {
+  //      m_MissedTailCount++;
+  //    }
+  //    if ((m_MissedTailCount + m_VisitedTails.size()) >= m_PuzzlePtr->GetTails().size()) {
+  //      return false;
+  //    }
+  //  }
+  //}
 
   // Find all the unvisited sides in segment if needed
   // TODO: add conditions
